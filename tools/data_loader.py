f05094c7159a1023df2a4d5f6e8b6480c8513f6c
Cassandra M. Sullivan <caseymr96@gmail.com>
2026-05-07 10:57:49 -0700

    Delete tools/data_loader.py

--- tools/data_loader.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from typing import Tuple, List
-
-def load_dataset(dataset_path: str) -> Tuple[pd.DataFrame, List[str]]:
-   df = pd.read_csv(dataset_path)
-   return df, df.columns.tolist()
